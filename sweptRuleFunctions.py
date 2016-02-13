0177c04adfc540ab5915b75b7eeea00df50346ae
TheLinspacer <danielm2967@gmail.com>
2016-02-13 00:27:40 -0800

    bottomTriangle basically works now.  I still need to figure out how to handle the edges though.  And how to collect everything at the end.  And how to pass right.  But it feels like it's 75 % of the way there.

--- sweptRuleFunctions.py
+++ sweptRuleFunctions.py
@@ -2,35 +2,50 @@
 
 
 def communication(vect, stage):
-    #When it isn't divisible by 2, it's true.  Dammit.  I need to get only the top parts to communicate.
-    if stage % 2:
-        Comm = [vect[t][-2:] for t in range(len(vect))]
-    else:
-        Comm = [vect[t][:2] for t in range(len(vect))]
 
-    return Comm
+    # When it isn't divisible by 2, it's true.  Dammit.  I need to get only the top parts to communicate.
+    if stage % 2:
+        comm = [vect[t][-2:] for t in range(len(vect))]
+    else:
+        comm = [vect[t][:2] for t in range(len(vect))]
+
+    return comm
+
 
 def topTriangle(F, IC):
+
     out_a = [IC]
     while len(out_a[-1]) > 2:
 
         holder = [F * (out_a[-1][n - 1] + out_a[-1][n + 1] - 2 * out_a[-1][n]) + out_a[-1][n] for n in range(1, len(out_a[-1]) - 1)]
         out_a += [holder]
 
-
     return out_a
 
-def bottomTriangle(F, IC, l_nodes):
-    out_a = IC[0].extend(IC[-1])
-    while len(out_a[-1]) < l_nodes:
+
+def bottomTriangle(F, IC, l_nodes, ed):
+
+    out_a = IC[0]
+    out_a.extend(IC[-1])
+    out_a = [out_a]
+
+    while True:
+
         holder = [F * (out_a[-1][n - 1] + out_a[-1][n + 1] - 2 * out_a[-1][n]) +
                   out_a[-1][n] for n in range(1, len(out_a[-1]) - 1)]
+
+        if len(holder) == l_nodes:
+
+            if ed:
+                return out_a
+
+            out_a += [topTriangle(F,holder)]
+            return out_a
+
         holder.reverse()
         holder.extend(IC[len(out_a)])
         holder.reverse()
-        holder.extend(IC[-len(out_a)])
+        holder.extend(IC[-len(out_a)+1])
         out_a += [holder]
 
-    out_a += [topTriangle(F,out_a[-1])]
 
-    return out_a
--- sweptRuleMain.py
+++ sweptRuleMain.py
@@ -1,28 +1,22 @@
-
 import numpy as np
 from sweptRuleFunctions import *
 import math
 import matplotlib.pyplot as plt
 
-#Initial conditions.  Material = Aluminum #And Fire
-x = [t/.2 for t in range(50)]
+# Initial conditions.  Material = Aluminum
+x = [t * .2 for t in range(50)]
 node = 5
-l_nodes = len(x)/node
-Tbar = [(500.0*math.exp(-x/5.0)) for x in x] #Temperature along the bar in C
-#ID = Tbar.reshape((node,l_nodes))
-print Tbar
+l_nodes = len(x) / node
+# Temperature along the bar in C
+ID = [[(500.0 * math.exp(-x[(m*l_nodes+n)] / 5.0)) for n in range(l_nodes)] for m in range(node)]
 
-#print type(ID)
+Tri_ht = l_nodes / 2
 
-Tri_ht = l_nodes/2
+Alal = 8.418e-5  # Thermal diffusivity. in m^2/s
+kAl = 247.0  # Thermal Conductivity in W/mK
+dt = 10  # delta T in seconds
 
-# plt.plot(x,Tbar)
-# plt.show()
-Alal = 8.418e-5 #Thermal diffusivity. in m^2/s
-kAl = 247.0 #Thermal Conductivity in W/mK
-dt = 10 #delta T in seconds
-
-Fo = dt*Alal/(x[1]**2)
+Fo = dt * Alal / (x[1] ** 2)
 
 ending = 5
 
@@ -32,18 +26,31 @@ for k in range(ending):
     Nodes = []
     for n in range(node):
         if k == 0:
-            Nodes += [topTriangle(Fo, ID[n,:])]
-            print len(Nodes)
-        else:
-            print ID[n], len(ID)
+            Nodes += [topTriangle(Fo, ID[n][:])]
 
-            Nodes += bottomTriangle(Fo,ID[n], l_nodes)
+        else:
+            if k % 2:
+
+                if n == node-1:
+                    g = n
+                    n = 0
+                    IDb = ID[1][n][::-1] + ID[0][n+1]
+                    n = node-1
+                else:
+                    IDb = ID[1][n][::-1] + ID[0][n+1]
+                    print ID[1][n], ID[0][n+1]
+                    print IDb
+
+            else:
+                IDb = ID[0][n+1] + ID[1][n]
+
+            Nodes += bottomTriangle(Fo, IDb, l_nodes, k == ending-1)
 
     Full_Nodes += Nodes
-    ID = [communication(Nodes[m], k+1) for m in range(node)]
-    print ID[1]
+    # The communication.  So ID[0] is the communication part and ID[1] is the keeping part.
+    ID = [[communication(Nodes[-m], k + 1) for m in range(1,node+1)]]
+    # The keeping
+    ID += [[communication(Nodes[-m], k) for m in range(1,node+1)]]
+
 
 print 'Done'
-
-
-
